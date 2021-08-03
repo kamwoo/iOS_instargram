5229ba9583971d3a57d654453d77626e36406640
kamwoo <rkadndud06@naver.com>
2021-08-03 21:04:13 +0900

    Initial Commit

---
+++ iOS_instargram/Controllers/Core Tabs/CameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.swift
+//  iOS_instargram
+//
+//  Created by wooyeong kam on 2021/08/03.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ iOS_instargram/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewController.swift
+//  iOS_instargram
+//
+//  Created by wooyeong kam on 2021/08/03.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
